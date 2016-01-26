5e18864735e5ccd44a267ec1a47c873ef4693e92
Mickey Muzi Li <mickeylee524@gmail.com>
2016-01-25 16:01:06 -0800

    relative path taken as params

--- fileScrubber.py
+++ fileScrubber.py
@@ -17,16 +17,25 @@ def main(dir, csvfileName):
 
 	for root, dirs, files in os.walk(dir):
 		for name in files:
-			if name in fileNames:
+			allPath = os.path.join(root, name)
+			relPath = os.path.relpath(allPath, dir)
+			# print (root)
+			# print (relPath)
+			# print (allPath)
+
+			if relPath in fileNames:
 				# print ('yes')
 				# print ('files:')
 				print ('Removing file:', os.path.join(root, name))
 				os.remove(os.path.join(root, name))
 
 		for name in dirs:
-			if name in folderNames:
+			allPath = os.path.join(root, name)
+			relPath = os.path.relpath(allPath, dir)
+			# print (relPath)
+			if relPath in folderNames:
 				print ('Removing folder', os.path.join(root, name))
-				os.removedirs(os.path.join(root, name))
+				os.rmdir(os.path.join(root, name))
 				
 	return 'Scrubbing finished'
 if __name__ == '__main__':
